8d659f881d347a07096ea7ec3adf221eeefa322c
Deison <deisoncar67@gmail.com>
2025-09-03 14:10:12 -0500

    inventario-app

---
+++ src/routes.js
@@ -0,0 +1,45 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.2.4
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2024 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import Index from "views/Index.js";
+import Login from "views/examples/Login.js";
+import Proveedor from "views/pages/proveedor/Index";
+
+var routes = [
+  {
+    path: "/index",
+    name: "Dashboard",
+    icon: "ni ni-tv-2 text-primary",
+    component: <Index />,
+    layout: "/admin",
+  },
+  {
+    path: "/proveedor",
+    name: "Proveedor",
+    icon: "ni ni-watch-time text-primary",
+    component: <Proveedor />,
+    layout: "/admin",
+  },
+  {
+    path: "/login",
+    name: "Login",
+    icon: "ni ni-key-25 text-info",
+    component: <Login />,
+    layout: "/auth",
+  }
+];
+export default routes;
